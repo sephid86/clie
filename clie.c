dc4ac4fc97cff54e35b6561330a1b82197e0008c
sephid86 <sephid86@gmail.com>
2022-04-25 03:11:07 +0900

    220425

--- clie.c
+++ clie.c
@@ -23,6 +23,10 @@
 //전역변수는 g_스네이크 표기법으로 한다.
 //괄호는 K&R 스타일로 한다.
 //들여쓰기는 스페이스바 2칸 으로 한다.
+typedef struct {
+  GtkWidget *TextView;
+  char recv_str[BUFFER_SIZE];
+} MyWidgetGroup;
 
 int g_sockfd;
 GtkTextBuffer *Buffer;
@@ -61,11 +65,13 @@ int main(int argc, char *argv[]) {
   GtkWidget *Layout1=gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
 
   //---여러줄 글상자 설정
-  GtkWidget *TextView=gtk_text_view_new();
-  Buffer=gtk_text_view_get_buffer(GTK_TEXT_VIEW(TextView));
-  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(TextView), 1); 
+  //GtkWidget *TextView=gtk_text_view_new();
+  MyWidgetGroup *MyWidget=malloc(sizeof(MyWidgetGroup));
+  MyWidget->TextView=gtk_text_view_new();
+  Buffer=gtk_text_view_get_buffer(GTK_TEXT_VIEW(MyWidget->TextView));
+  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(MyWidget->TextView), 1); 
   //gtk_widget_set_size_request(TextView, -1,350);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(TextView), 0);
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(MyWidget->TextView), 0);
 
   //---여러줄 글상자 스크롤 설정
   GtkWidget *ScrollTextview=gtk_scrolled_window_new(NULL, NULL);
@@ -77,7 +83,7 @@ int main(int argc, char *argv[]) {
 
   //---컨테이너 등록
   gtk_container_add(GTK_CONTAINER(Window), Layout1);
-  gtk_container_add(GTK_CONTAINER(ScrollTextview), TextView);
+  gtk_container_add(GTK_CONTAINER(ScrollTextview), MyWidget->TextView);
   gtk_box_pack_start(GTK_BOX(Layout1),ScrollTextview,1,1,0);
   gtk_container_add(GTK_CONTAINER(Layout1), Entry);
 
@@ -86,7 +92,7 @@ int main(int argc, char *argv[]) {
   g_signal_connect(Entry, "activate", G_CALLBACK(EntryActivate), NULL);
 
   //---쓰레드 생성(소켓리시브)
-  GThread *RecvThread=g_thread_new(NULL,(GThreadFunc)SockRecv,TextView);
+  GThread *RecvThread=g_thread_new(NULL,(GThreadFunc)SockRecv,MyWidget);
 
   //---화면에 윈도우 출력
   gtk_widget_show_all(Window);
@@ -101,33 +107,43 @@ void EntryActivate(GtkEntry *Entry, GtkTextView *TextView) {
   gtk_entry_set_text(GTK_ENTRY(Entry), "");
 }
 
-void *SockRecv(GtkWidget *TextView) {
-    GtkTextIter end_iter;
-    gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(Buffer), &end_iter);
-    //GtkTextMark *mark = gtk_text_buffer_create_mark(GTK_TEXT_BUFFER(Buffer), NULL, &end_iter, 1);
+void *SockRecv(MyWidgetGroup *MyWidget) {
+  //GtkTextIter end_iter;
+  //gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(Buffer), &end_iter);
+  //GtkTextMark *mark = gtk_text_buffer_create_mark(GTK_TEXT_BUFFER(Buffer), NULL, &end_iter, 1);
 
-  char recv_str[BUFFER_SIZE];
+  //char recv_str[BUFFER_SIZE];
+  int tmp;
   while(1) {
-    //---수신 버퍼를 비우고 수신함
-    memset(&recv_str, 0, BUFFER_SIZE);
-    read(g_sockfd, recv_str, BUFFER_SIZE);
-    g_idle_add((GSourceFunc)WidgetShowSafe,TextView);
-    //---수신된 버퍼를 여러줄 글상자에 출력
-    gtk_text_buffer_insert(GTK_TEXT_BUFFER(Buffer), &end_iter, recv_str,-1);
-    //---스크롤을 마지막 버퍼로 이동함
-    //gtk_text_buffer_move_mark(GTK_TEXT_BUFFER(Buffer), mark, &end_iter);
-    //gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(TextView), mark);
+    tmp=read(g_sockfd, MyWidget->recv_str, BUFFER_SIZE);
+    if(tmp) {
+      g_idle_add((GSourceFunc)WidgetShowSafe,MyWidget);
+      //---수신된 버퍼를 여러줄 글상자에 출력
+      //gtk_text_buffer_insert(GTK_TEXT_BUFFER(Buffer), &end_iter, recv_str,-1);
+      //---스크롤을 마지막 버퍼로 이동함
+      //gtk_text_buffer_move_mark(GTK_TEXT_BUFFER(Buffer), mark, &end_iter);
+      //gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(TextView), mark);
+    }
   }
 }
 
-gboolean WidgetShowSafe(GtkWidget *TextView) {
+gboolean WidgetShowSafe(MyWidgetGroup *MyWidget) {
   GtkTextIter end_iter;
   gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(Buffer), &end_iter);
   GtkTextMark *mark = gtk_text_buffer_create_mark(GTK_TEXT_BUFFER(Buffer), NULL, &end_iter, 1);
 
   //---수신된 버퍼를 여러줄 글상자에 출력
-  //gtk_text_buffer_insert(GTK_TEXT_BUFFER(Buffer), &end_iter, recv_str,-1);
+  gtk_text_buffer_insert(GTK_TEXT_BUFFER(Buffer), &end_iter, MyWidget->recv_str,-1);
   //---스크롤을 마지막 버퍼로 이동함
   gtk_text_buffer_move_mark(GTK_TEXT_BUFFER(Buffer), mark, &end_iter);
-  gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(TextView), mark);
+  gtk_text_view_scroll_mark_onscreen(GTK_TEXT_VIEW(MyWidget->TextView), mark);
 }
+
+//위 소스코드를 살펴보면 SockRecv 에서 주석처리된 함수들이
+//WidgetShowSafe 함수로 이동한것을 알수 있습니다.
+//즉 쓰레드에서 직접 GTK 함수를 호출하는 것은 위험합니다.
+//쓰레드에서 GTK 함수를 호출하려면 g_idle_add 함수를 통해야 합니다.
+//그리고 GUI 화면출력과 관련된 GTK 함수가 아니라면.
+//예를 들면 widget 의 내용을 변경하는 GTK 함수 같은것들은
+//쓰레드에서 직접 호출해도 왠만해선 문제가 없는것 같습니다.
+
